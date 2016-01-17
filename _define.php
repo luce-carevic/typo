791920549b7d58e789bdcdeb701cc37200e81b64
franck <carnet.franck.paul@gmail.com>
2016-01-17 10:41:29 +0100

    PHP 7 compliance of SmartyPants library

--- _define.php
+++ _define.php
@@ -15,7 +15,7 @@ $this->registerModule(
 	/* Name */				"Typo",
 	/* Description*/		"Brings smart typographic replacements for your blog entries and comments",
 	/* Author */			"Franck Paul",
-	/* Version */			'1.8',
+	/* Version */			'1.8.1',
 	array(
 		/* Permissions */	'permissions' =>	'contentadmin',
 		/* Type */			'type' =>			'plugin'
--- inc/smartypants.php
+++ inc/smartypants.php
@@ -12,8 +12,8 @@
 #
 
 
-define( 'SMARTYPANTS_VERSION',  "1.5.1f" ); # Sun 23 Jan 2013
-define( 'SMARTYPANTSTYPOGRAPHER_VERSION',  "1.0.1" ); # Sun 23 Jan 2013
+define( 'SMARTYPANTS_VERSION',  "1.5.1f-php7" ); # Sun 23 Jan 2013
+define( 'SMARTYPANTSTYPOGRAPHER_VERSION',  "1.0.1-php7" ); # Sun 23 Jan 2013
 
 
 #
@@ -164,7 +164,7 @@ class SmartyPants_Parser {
 	var $do_stupefy   = 0;
 	var $convert_quot = 0; # should we translate &quot; entities into normal quotes?
 
-	function SmartyPants_Parser($attr = SMARTYPANTS_ATTR) {
+	function __construct($attr = SMARTYPANTS_ATTR) {
 	#
 	# Initialize a SmartyPants_Parser with certain attributes.
 	#
@@ -634,7 +634,7 @@ class SmartyPantsTypographer_Parser extends SmartyPants_Parser {
 
 
 
-	function SmartyPantsTypographer_Parser($attr = SMARTYPANTS_ATTR) {
+	function __construct($attr = SMARTYPANTS_ATTR) {
 	#
 	# Initialize a SmartyPantsTypographer_Parser with certain attributes.
 	#
@@ -670,7 +670,7 @@ class SmartyPantsTypographer_Parser extends SmartyPants_Parser {
 	#    sign to completly remove any space present)
 	#
 		# Initialize inherited SmartyPants parser.
-		parent::SmartyPants_Parser($attr);
+		parent::__construct($attr);
 
 		if ($attr == "1" || $attr == "2" || $attr == "3") {
 			# Do everything, turn all options on.
@@ -1183,4 +1183,3 @@ arising in any way out of the use of this software, even if advised of the
 possibility of such damage.
 
 */
-?>
